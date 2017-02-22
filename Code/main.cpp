0b9d225df8cf1c9090643ca44b741b6fa9fc8788
AntonioBarral <abarrg02@estudiantes.unileon.es>
2017-02-22 17:30:42 +0100

    Add files via upload

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,27 +1,46 @@
-#include "mainwindow.h"
-#include <QApplication>
-#include <QSurfaceFormat>
+//
+//  Framework for a raytracer
+//  File: main.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include "raytracer.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    cout << "Introduction to Computer Graphics - Raytracer" << endl << endl;
+    if (argc < 2 || argc > 3) {
+        cerr << "Usage: " << argv[0] << " in-file [out-file.png]" << endl;
+        return 1;
+    }
 
-    // Request OpenGL 3.3 Core
-    QSurfaceFormat glFormat;
-    glFormat.setProfile(QSurfaceFormat::CoreProfile);
-    glFormat.setVersion(3, 3);
-    glFormat.setOption(QSurfaceFormat::DebugContext);
+    Raytracer raytracer;
 
-    // Some platforms need to explicitly set the depth buffer size (24 bits)
-    glFormat.setDepthBufferSize(24);
+    if (!raytracer.readScene(argv[1])) {
+        cerr << "Error: reading scene from " << argv[1] << " failed - no output generated."<< endl;
+        return 1;
+    }
+    std::string ofname;
+    if (argc>=3) {
+        ofname = argv[2];
+    } else {
+        ofname = argv[1];
+        if (ofname.size()>=5 && ofname.substr(ofname.size()-5)==".yaml") {
+            ofname = ofname.substr(0,ofname.size()-5);
+        }
+        ofname += ".png";
+    }
+    raytracer.renderToFile(ofname);
 
-    // Set super sampling if you want
-    //glFormat.setSamples(16);
-
-    QSurfaceFormat::setDefaultFormat(glFormat);
-
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    return 0;
 }
